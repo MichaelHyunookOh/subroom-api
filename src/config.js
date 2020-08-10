3282b5f618436d976e94d68990e57a839bc5d2f7
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-08-10 12:03:45 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://cowpkyawklzqmv:2567010323e7fa26aa9e14bc580a059d5c7a217aa13b652db2fd4f3198368c72@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d6or5lo3gikbkc',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://cowpkyawklzqmv:2567010323e7fa26aa9e14bc580a059d5c7a217aa13b652db2fd4f3198368c72@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d6or5lo3gikbkc?sslmode=required',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
 }
