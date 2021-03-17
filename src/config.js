21ffe7aa70a3b3ab50432bf5b8b8a8b3ecde7ab0
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2021-03-17 16:47:10 -0700

    update

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    'postgres://cowpkyawklzqmv:2567010323e7fa26aa9e14bc580a059d5c7a217aa13b652db2fd4f3198368c72@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d6or5lo3gikbkc',
+    'postgres://idqifucxsjhhoz:a48196bde405e27835cd9d93aa4195e686591b992fca7de6ed58af0f1cf960d7@ec2-3-91-127-228.compute-1.amazonaws.com:5432/d879rgblnafdku',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || '8cdb63a3-0226-4382-b0cd-3dd96e09fe4c',
 };
