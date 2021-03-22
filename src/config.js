9703ab1244c89f43e6dbfd4c09d597bca1def034
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2021-03-22 12:53:01 -0700

    purge

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    'postgres://idqifucxsjhhoz:a48196bde405e27835cd9d93aa4195e686591b992fca7de6ed58af0f1cf960d7@ec2-3-91-127-228.compute-1.amazonaws.com:5432/d879rgblnafdku',
+    'postgres://muzrktxcgpmhbw:21a6805f206eb52c4f2941af58b3163efcb6ab66a5da33c50b6586ce542c9941@ec2-54-159-175-113.compute-1.amazonaws.com:5432/dfjg9v491j1uhd',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || '8cdb63a3-0226-4382-b0cd-3dd96e09fe4c',
 };
