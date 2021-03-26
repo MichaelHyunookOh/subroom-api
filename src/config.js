ba2dd442846de5778d824c7032c2c6304ae0fb44
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2021-03-26 14:06:13 -0700

    testing

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    'postgres://muzrktxcgpmhbw:21a6805f206eb52c4f2941af58b3163efcb6ab66a5da33c50b6586ce542c9941@ec2-54-159-175-113.compute-1.amazonaws.com:5432/dfjg9v491j1uhd',
+    'postgres://zoxkclcc:Gta32eD-JVHl-Omj49FHEi1mo-rRye-z@kashin.db.elephantsql.com:5432/zoxkclcc',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || '8cdb63a3-0226-4382-b0cd-3dd96e09fe4c',
 };
